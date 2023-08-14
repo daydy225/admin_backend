07bac4e15301ced1d138b403fada018935334049
daydy225 <dabelydadi@gmail.com>
2023-08-14 06:28:00 +0000

    Save Frontend add static frontend file

--- src/app.js
+++ src/app.js
@@ -12,8 +12,10 @@ const app = express()
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 
-app.use(helmet())
 app.use(cors())
+app.use(helmet())
+
+app.use(express.static('dist'))
 
 app.get('/', (req, res) => {
   res.json({ message: 'Society admin API, Access unauthorized' })
